26d8113e927b0f3b5b91cf64c34d34a5a38c5afa
Nealadam1 <Nealadam@live.com>
2022-12-05 17:07:31 +0200

    First Commit

---
+++ guess-me/js/quest.service.js
@@ -0,0 +1,53 @@
+'use strict'
+const STORAGE_KEY = 'questDB'
+
+var gQuestsTree
+var gCurrQuest
+var gPrevQuest = null
+
+function createQuestsTree() {
+  gQuestsTree = loadFromStorage(STORAGE_KEY)
+  if (!gQuestsTree) {
+    gQuestsTree = createQuest('Male?')
+    gQuestsTree.yes = createQuest('Gandhi')
+    gQuestsTree.no = createQuest('Rita')
+    saveToStorage(STORAGE_KEY,gQuestsTree)
+  }
+  gCurrQuest = gQuestsTree
+  gPrevQuest = null
+}
+
+function createQuest(txt) {
+  return {
+    txt: txt,
+    yes: null,
+    no: null,
+  }
+}
+
+function isChildless(node) {
+  return node.yes === null && node.no === null
+}
+
+function moveToNextQuest(res) {
+  // TODO: update the gPrevQuest, gCurrQuest global vars
+  gPrevQuest = gCurrQuest
+  gCurrQuest = gPrevQuest[res]
+
+}
+
+function addGuess(newQuestTxt, newGuessTxt, lastRes) {
+  // TODO: Create and Connect the 2 Quests to the quetsions tree
+  var newQuest = createQuest(newQuestTxt)
+  newQuest.yes = createQuest(newGuessTxt)
+  newQuest.no = gCurrQuest
+  gPrevQuest[lastRes] = newQuest
+
+  saveToStorage(STORAGE_KEY, gQuestsTree)
+
+
+}
+
+function getCurrQuest() {
+  return gCurrQuest
+}
